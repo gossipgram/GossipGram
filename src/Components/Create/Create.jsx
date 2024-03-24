4f8b9ea14bdb82b2e4d6fa30b35465cacedfbe45
kunal1632 <kunal.dk35@gmail.com>
2024-03-25 01:57:58 +0530

    uploading image option added

--- src/Components/Create/Create.jsx
+++ src/Components/Create/Create.jsx
@@ -5,16 +5,9 @@ import CreateForm from "./CreateForm";
 const Create = () => {
   const [postType, setpostType] = useState("");
 
-  const textButtonHandler = () => {
-    setpostType("text");
+  const radioChangeHandler = (event) => {
+    setpostType(event.target.value);
   };
-  const photoButtonHandler = () => {
-    setpostType("photo");
-  };
-  const videoButtonHandler = () => {
-    setpostType("video");
-  };
-
   return (
     <div className="w-full flex items-center flex-col gap-1 h-auto ">
       <div className="bg-richblack-700 w-1/2 h-1/6  flex gap-5 flex-col items-center p-3 rounded-3xl border border-richblack-400">
@@ -23,31 +16,59 @@ const Create = () => {
         </h2>
 
         <div className="flex gap-24 mb-4">
-          <button
-            onClick={textButtonHandler}
-            className="text-richblack-5 text-lg border border-richblack-100 px-9  py-2 rounded-full bg-richblack-600 hover:text-yellow-300 hover:border-yellow-300 focus:outline-none focus:text-yellow-300 focus:border-yellow-300 transition-all duration-300"
+          <label
+            className={` ${
+              postType === "text" ? "text-yellow-300" : " text-richblack-25"
+            } bg-richblack-600 text-lg border rounded-full px-9 cursor-pointer py-2 hover:text-yellow-300 transition-all duration-300`}
           >
-            Text
-          </button>
+            <input
+              type="radio"
+              value="text"
+              checked={postType === "text"}
+              onChange={radioChangeHandler}
+              className="sr-only"
+            />
+            <span>Text</span>
+          </label>
+
           <div className="w-[1px] h-full bg-richblack-100 "></div>
-          <button
-            onClick={photoButtonHandler}
-            className="text-richblack-5 text-lg border border-richblack-100 px-7 py-2 rounded-full bg-richblack-600 hover:text-yellow-300 hover:border-yellow-300 focus:outline-none focus:text-yellow-300 focus:border-yellow-300 transition-all duration-300"
+
+          <label
+            className={` ${
+              postType === "photo" ? "text-yellow-300" : " text-richblack-25"
+            } bg-richblack-600 text-lg border rounded-full px-7 cursor-pointer py-2 hover:text-yellow-300 transition-all duration-300 `}
           >
-            Photo
-          </button>
+            <input
+              type="radio"
+              value="photo"
+              checked={postType === "photo"}
+              onChange={radioChangeHandler}
+              className="sr-only"
+            />
+            <span>Photo</span>
+          </label>
+
           <div className="w-[1px] h-full bg-richblack-100 "></div>
-          <button
-            onClick={videoButtonHandler}
-            className="text-richblack-5 text-lg border border-richblack-100 px-7 py-2 rounded-full bg-richblack-600 hover:text-yellow-300 hover:border-yellow-300 focus:outline-none focus:text-yellow-300 focus:border-yellow-300 transition-all duration-300"
+
+          <label
+            className={` ${
+              postType === "video" ? "text-yellow-300" : " text-richblack-25"
+            } bg-richblack-600 text-lg border rounded-full px-7 cursor-pointer py-2 hover:text-yellow-300 transition-all duration-300 `}
           >
-            Video
-          </button>
+            <input
+              type="radio"
+              value="video"
+              checked={postType === "video"}
+              onChange={radioChangeHandler}
+              className="sr-only"
+            />
+            <span>Video</span>
+          </label>
         </div>
       </div>
 
       <div className="w-full h-full flex items-center justify-center">
-        <CreateForm />
+        <CreateForm postType={postType} />
       </div>
     </div>
   );
--- src/Components/Create/CreateForm.jsx
+++ src/Components/Create/CreateForm.jsx
@@ -1,9 +1,76 @@
 import React from "react";
+import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
 
-const CreateForm = () => {
+const CreateForm = ({ postType }) => {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (event) => {
+    const selectedImage = event.target.files[0];
+    setImage(selectedImage);
+    console.log(image);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const droppedFile = event.dataTransfer.files;
+    const droppedImage = droppedFile[0];
+    if (droppedImage && droppedImage.type.startsWith("image/")) {
+      setImage(droppedImage);
+    } else {
+      alert("Please select Image");
+    }
+  };
+
+  const handleCrossButton = () => {
+    setImage(null);
+  };
   return (
-    <div className="bg-richblack-700 w-11/12 flex items-center justify-around h-5/6  rounded-3xl p-5">
-      <div className="bg-white">image upload</div>
+    <div className="bg-richblack-700 w-11/12 flex items-center justify-between h-5/6  rounded-3xl p-5">
+      <div
+        className="w-1/3 flex items-center justify-center h-5/6 ml-16 rounded-3xl border-4 border-dashed border-richblack-400"
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        {image ? (
+          <div className="w-full h-full relative">
+            <img
+              src={URL.createObjectURL(image)}
+              alt="image"
+              className="w-full h-full object-cover  rounded-xl z-20"
+            />
+            <button
+              className="z-30 text-2xl text-richblack-25 absolute top-4 right-4 bg-richblack-700 rounded-full bg-opacity-20 p-1"
+              onClick={handleCrossButton}
+            >
+              <RxCross2 />
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-3 flex-col items-center justify-center">
+            <span className="text-richblack-100 text-xl">
+              Select your image or drop here
+            </span>
+            <label
+              htmlFor="imageFile"
+              className="text-richblack-5 cursor-pointer text-lg px-7 py-2  rounded-xl bg-yellow-400 hover:bg-yellow-500 transition-all duration-300"
+            >
+              Select
+            </label>
+            <input
+              type="file"
+              accept="image/*"
+              id="imageFile"
+              onChange={handleImageChange}
+              className="hidden"
+            />
+          </div>
+        )}
+      </div>
 
       <div className="bg-white">caption and hashtag</div>
     </div>
