53e75e11132c376748242614ea83345e74d09930
kunal1632 <kunal.dk35@gmail.com>
2024-04-02 14:45:56 +0530

    profile work started

--- server/models/User.js
+++ server/models/User.js
@@ -1,90 +1,98 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-    {
-        firstName: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-		lastName: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-        username:{
-            type:String,
-            required:true,
-            trim:true,
-        },
-		// Define the email field with type String, required, and trimmed
-		email: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-
-		// Define the password field with type String and required
-		password: {
-			type: String,
-			required: true,
-		},
-
-	           //solwe this issue later on confirm password
-		
-        token: {
-			type: String,
-		},
-        additionalDetails: {
-			type: mongoose.Schema.Types.ObjectId,
-			required: true,
-			ref: "Profile",
-		},
-        image: {
-			type: String,
-			required: true,
-		},
-		recentSearches: {
-            type: [{
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User'
-            }],
-        // unique: true // Ensure only unique elements in the recentSearches array
-        },
-        // bio: {                        //already added to AdditionalDetails
-		// 	type: String,        
-		// 	required: true,
-		// },
-
-        // Relationship with Posts
-        posts: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Post'
-        }],
-
-        // Relationship with Comments
-        likes: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Like'
-        }],
-
-        // Relationship with Followers
-        followers: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Follower'
-        }],
-
-        // Relationship with Following
-        following: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Follower'
-        }],
-		// likes: [{
-		// 	type: mongoose.Schema.Types.ObjectId,
-		// 	ref: "likes"
-		// }]
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    { timestamps: true }
-)
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    // Define the email field with type String, required, and trimmed
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-module.exports = mongoose.model('User', userSchema);
+    // Define the password field with type String and required
+    password: {
+      type: String,
+      required: true,
+    },
+
+    //solwe this issue later on confirm password
+
+    token: {
+      type: String,
+    },
+    additionalDetails: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Profile",
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    recentSearches: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    // unique: true // Ensure only unique elements in the recentSearches array
+    // bio: {                        //already added to AdditionalDetails
+    // 	type: String,
+    // 	required: true,
+    // },
+
+    // Relationship with Posts
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+
+    // Relationship with Comments
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Like",
+      },
+    ],
+
+    // Relationship with Followers
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Follower",
+      },
+    ],
+
+    // Relationship with Following
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Follower",
+      },
+    ],
+    // likes: [{
+    // 	type: mongoose.Schema.Types.ObjectId,
+    // 	ref: "likes"
+    // }]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import UserProfile from "../Components/Profile/UserProfile";
+import { getAllUserData } from "../services/operations/profileAPI";
+import { useState, useEffect } from "react";
 
 const Profile = () => {
+  const [userData, setUserData] = useState(null);
+  const token = localStorage.getItem("token").split('"')[1];
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getAllUserData(token);
+        setUserData(response);
+        // setRecentSearches(response?.userDetails?.recentSearches);
+      } catch (error) {
+        console.error("Error fetching user data:", error.message);
+      }
+    };
+    if (token) {
+      fetchUserData();
+    }
+  }, [token]);
+  const userId = userData?._id;
+
   return (
     <div>
-      <h1>hello</h1>
+      {/* <UserProfile
+      // userData={userData}
+      // userId={userId}
+      // matchingUsers={matchingUsers}
+      // handleSearchItemClick={handleSearchItemClick}
+      /> */}
     </div>
   );
 };
