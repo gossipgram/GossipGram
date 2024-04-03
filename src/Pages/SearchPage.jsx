8107ab5648f6adeda2d174294c78a6bf75149a91
kunal1632 <kunal.dk35@gmail.com>
2024-04-03 21:14:17 +0530

    recent fix and working

--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -45,20 +45,6 @@ const SearchPage = () => {
   }, [token, userId]);
 
   useEffect(() => {
-    const fetchAllUsers = async () => {
-      try {
-        const res = await getAllUsers(token);
-        setAllUsers(res.users);
-      } catch (error) {
-        console.error("Error fetching user data:", error.message);
-      }
-    };
-    if (token) {
-      fetchAllUsers();
-    }
-  }, [token, userId]);
-
-  useEffect(() => {
     const fetchUserData = async () => {
       try {
         const response = await getAllUserData(token);
