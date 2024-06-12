8d7712c7346b582f38d602629b8073bbff2a8823
SHASHANK2405 <shashankgulati2405@gmail.com>
2024-06-12 20:34:07 +0530

    updated

--- src/Components/Profile/UserProfile.jsx
+++ src/Components/Profile/UserProfile.jsx
@@ -322,49 +322,57 @@ const UserProfile = ({ userId, matchingUsers, userData }) => {
  
 
       <div className="w-full h-[1px] bg-yellow-500 mt-12"></div>
-
-      <div className="flex w-full justify-center gap-32">
-        {steps.map((item) => (
-          <div className="flex flex-col items-center" key={item.id}>
-            <button
-              className={`grid cursor-pointer aspect-rectangle w-[70px] h-[40px] place-items-center rounded-xl border-[1px] mt-3 p-5${
-                postSection === item.title
-                  ? "border-yellow-50 bg-yellow-900 text-yellow-50 transition-all duration-200"
-                  : "border-richblack-700 bg-richblack-800 text-richblack-300 hover:text-yellow-400 transition-all duration-200"
-              } ${step > item.id && "bg-yellow-50 text-yellow-50"}`}
-              onClick={() => setPostSection(item.title)}
-            >
-              {item.title}
-            </button>
+      {!userId.isPrivate || isFollowing ?
+        (
+          <div>
+          <div className="flex w-full justify-center gap-32">
+            {steps.map((item) => (
+              <div className="flex flex-col items-center" key={item.id}>
+                <button
+                  className={`grid cursor-pointer aspect-rectangle w-[70px] h-[40px] place-items-center rounded-xl border-[1px] mt-3 p-5${
+                    postSection === item.title
+                      ? "border-yellow-50 bg-yellow-900 text-yellow-50 transition-all duration-200"
+                      : "border-richblack-700 bg-richblack-800 text-richblack-300 hover:text-yellow-400 transition-all duration-200"
+                  } ${step > item.id && "bg-yellow-50 text-yellow-50"}`}
+                  onClick={() => setPostSection(item.title)}
+                >
+                  {item.title}
+                </button>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="w-full h-[1px] bg-yellow-500 mt-5 mb-5"></div>
+          <div className="w-full h-[1px] bg-yellow-500 mt-5 mb-5"></div>
 
-      <div>
-        {" "}
-        {postSection === "Posts" ? (
-          <PostGrid
-            userId={userId}
-            searchedUserId={searchedUserId}
-            matchingUsers={matchingUsers}
-          />
-        ) : postSection === "Gossip" ? (
-          <PostRow
-            userData={userData}
-            searchedUser={userId}
-            searchedUserId={searchedUserId}
-          />
-        ) : postSection === "Tagged" ? (
-          <TaggedPost
-            userId={userId}
-            searchedUserId={searchedUserId}
-            matchingUsers={matchingUsers}
-          />
+          <div>
+            {" "}
+            {postSection === "Posts" ? (
+              <PostGrid
+                userId={userId}
+                searchedUserId={searchedUserId}
+                matchingUsers={matchingUsers}
+              />
+            ) : postSection === "Gossip" ? (
+              <PostRow
+                userData={userData}
+                searchedUser={userId}
+                searchedUserId={searchedUserId}
+              />
+            ) : postSection === "Tagged" ? (
+              <TaggedPost
+                userId={userId}
+                searchedUserId={searchedUserId}
+                matchingUsers={matchingUsers}
+              />
+            ) : (
+              <PostGrid />
+            )}
+          </div>
+          </div>
         ) : (
-          <PostGrid />
-        )}
-      </div>
+        <div className="text-center text-3xl mt-40 text-richblack-5 transition-all duration-700">
+          Private Account
+        </div>)
+      }
     </div>
   );
 };
